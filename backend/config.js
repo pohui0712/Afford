003450fdf363a831a71f00b395bacf67e0e451a5
pohui0712 <phxx04@gmail.com>
2024-05-31 14:14:30 +0800

    Connect with database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
 export const PORT = 5000;
+
+// mongodb
+export const mongoDBURL =
+  "mongodb+srv://pohui:Hui_11234@afford.c6vv5gv.mongodb.net/?retryWrites=true&w=majority&appName=Afford";
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.status(200).send("Connect successfully.");
+  return response.status(200).send("Hello World.");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connect databse successfully.");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
