7b424f0f1ed458523329202d8ebec8915b8c1a64
pohui0712 <phxx04@gmail.com>
2024-05-31 14:56:38 +0800

    frontend set-up

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
+
+// Middleware for pasesing JSON request body
+app.use(express.json());
+
+/// Middleware for handling CORS policy
+app.use(cors());
 
 app.get("/", (request, response) => {
   return response.status(200).send("Hello World.");
