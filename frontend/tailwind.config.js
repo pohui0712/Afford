c90690813a3072523c232fc116e513e22bf3aa99
FreshNgoh <jiankiat4486@gmail.com>
2024-06-05 17:23:58 +0800

    Merge branch 'Ngoh'

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,9 +21,23 @@ module.exports = {
           },
         },
       },
+      fontFamily: {
+        "pt-sans": ['"PT Sans"', "sans-serif"],
+      },
+      fontWeight: {
+        "pt-sans-regular": 400,
+        "pt-sans-bold": 700,
+      },
+      fontStyle: {
+        "pt-sans-italic": "italic",
+        "pt-sans-normal": "normal",
+      },
     },
   },
   plugins: [addVariablesForColors],
+  corePlugins: {
+    scrollBehavior: true,
+  },
 };
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
